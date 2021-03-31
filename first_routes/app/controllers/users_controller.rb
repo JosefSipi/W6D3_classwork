77130658e483ff36b6a6333d23ba394b2e0a7ba2
jackcusick95 <jackcusick95@gmail.com>
2021-03-31 17:34:19 -0400

    Add show, destroy, create actions

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -13,6 +13,21 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        user = User.find(params[:id])
+        render: json: user
+    end 
+
+    def destroy
+        user = User.find(params[:id]) 
+        user.destroy 
+    end 
+
+    def update
+        user = User.find(params[:id])
+        
+    end 
+
     private
 
     def user_params
