1d0f81995435e6ce049e38e5f8ae0e17be106f6b
Joseph <Josephsipiorski@gmail.com>
2021-03-31 16:03:24 -0500

    creating a user through the API

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        users = User.all
+        render json: users
     end
 
     def create
-        render json: params
+        user = User.new(user_params)
+        if user.save
+            render json: user   
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def user_params
+        debugger
+        params.require(:user).permit(:name, :email)
     end
 end 
